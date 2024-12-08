430252bb0ab7cabf1261bcfdbe33e2cf6e70e79e
Mikhail Zhukov <M-kh-l.i-ai-O@yandex.ru>
2024-12-08 18:36:09 +0300

    refac

--- alembic/env.py
+++ alembic/env.py
@@ -73,9 +73,8 @@ def do_run_migrations(connection):
     context.configure(
         connection=connection,
         target_metadata=target_metadata,
-        # Допишите ещё один параметр.
         render_as_batch=True,
-        )
+    )
 
     with context.begin_transaction():
         context.run_migrations()
